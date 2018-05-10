1ca75db0773f8e889d63a54df71091b277749b62
unknown <SavenTheChickens@gmail.com>
2018-05-10 00:11:32 -0600

    finished nesting practice

--- nesting.js
+++ nesting.js
@@ -81,14 +81,20 @@ var workplaceAccidents = [12, 56, 44, 3, 29, 56, 56, 3, 7, 12];
 */
 
 function removeDuplicates(){
-  var newArray = []
-  for(var i = 0; i < workplaceAccidents.length; i++){
-    for(var j = 0; j < newArray.length; j++){ 
-      if(workplaceAccidents[i] !== newArray[j]){
-        newArray.push(workplaceAccidents[i]);
-      }
+  var newArray = [];
+  for(var i = 0; i < workplaceAccidents.length; i++){   
+    var found = false;
+    for(var j = 0; j < workplaceAccidents.length; j++){ 
+      if(workplaceAccidents[i] === newArray[j]){
+         found = true;
+         break;                
+      }     
     }
+    if(found === false){
+      newArray.push(workplaceAccidents[i]);     
+    }    
   }
+  return newArray;
 }
 
 
@@ -118,8 +124,8 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1];
+var fluffy2ndFriend = cat.catFriends[1].name;
 
 
 
@@ -159,7 +165,12 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-//Code Here
+function recordCleaner(){
+  for(let i = 0; i < myCar.accidents.length; i++){
+      myCar.accidents[i].atFaultForAccident = false;
+  }
+  return myCar;
+}
 
 
 
@@ -178,6 +189,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
+function looper(){
+  for(let i = 0; i < numsArr.length; i++){
+    for(let j = 0; j <numsArr[i].length; j++){
+      if(numsArr[i][j] % 2 === 0){
+        numsArr[i][j] = "even"
+      } else
+      numsArr[i][j] = "odd"
+    }        
+  }
+  return numsArr;  
+}
 
 
